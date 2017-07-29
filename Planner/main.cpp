a72f8001848a3a054f8c7d5bebaff1071bbdf346
Chris O'Neill <chris.s.oneill@o2.co.uk>
2017-07-29 21:42:22 +0100

    Initial commit

---
+++ Planner/main.cpp
@@ -0,0 +1,19 @@
+// Daily Planner 2017 
+// Author : Chris O'Neill
+// File : src/main.cpp
+
+#include <iostream>
+
+using namespace std;
+
+bool g_bDebugMode = true;
+
+int main()
+{
+	//Open program
+	if (g_bDebugMode)cout << "Program Started" << endl;
+
+	//look for user profiles
+
+
+}
